96a59317b2fb235c2916e2b17570f814499dc3d9
Konrad Chmielewski <44814786+konradchmielewski@users.noreply.github.com>
2019-07-13 19:37:46 +0200

    Add files via upload
    initial commit

---
+++ OptionalPower/OptionalPower/wwwroot/js/optionalPow.js
@@ -0,0 +1,11 @@
+﻿var a, b;
+
+// Optional math power function, depending on the input - OK or Cancel.
+function optionalPow(a, b) {
+    if (confirm('Click >OK to Math.pow(a, b) and click Cancel to Math.pow(b, a)')) {
+        return Math.pow(a, b)
+    }
+    return Math.pow(b, a)
+}
+
+console.log(optionalPow(2, 3));
